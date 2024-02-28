d61de3af0d750574daec8eaf3a6a17da8d13ddb2
Marcin Warzybok <m.warzybok@sumoheavy.com>
2024-02-28 15:10:07 +0100

    SP-812 Build WooCommerce testing into the pipeline

--- .github/workflows/acceptance/acceptance-tests.sh
+++ .github/workflows/acceptance/acceptance-tests.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo "ACCEPTANCE TESTS"
 ls -lha
+pwd
 
-mysql -hapache -uroot -proot bitpay-wordpress-test <./.github/workflows/acceptance/dump.sql && echo "Restored Database"
+ls -lha /etc/
 
-
+mysql -hmysql -uroot -proot bitpay-wordpress-test <./.github/workflows/acceptance/dump.sql && echo "Restored Database"
 
 a2enmod rewrite
 systemctl apache2 status
